4ba7989e2d7ef542e7306b5e2cc442255368cf63
Till Prochaska <mail@tillprochaska.de>
2019-04-30 17:32:41 +0200

    Initial commit

---
+++ mergeData.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+
+const rawPolls = fs.readFileSync('./source/polls.json');
+const polls = JSON.parse(rawPolls);
+
+const rawDeputies = fs.readFileSync('./source/deputies.json');
+const deputies = JSON.parse(rawDeputies);
+
+const merged = polls.map(poll => {
+  let votes = poll.votes.deputies;
+  
+  votes = votes.map(({webId, vote}) => {
+    const dep = deputies.find(dep => dep.webId === webId);
+    
+    if(!dep) {
+      return {
+        name: 'Name nicht verfügbar',
+        constituency: 'Wahlkreis nicht verfügbar',
+        vote
+      };
+    }
+    
+    return {
+      name: dep.name,
+      constituency: dep.constituencyName,
+      vote: vote
+    };
+  });
+  
+  return {
+    title: poll.title,
+    date: poll.date,
+    votes: votes,
+  };
+});
+
+const indexJSON = merged.map((poll, index) => {
+  return {
+    id: index,
+    title: poll.title,
+    date: poll.date,
+  };
+});
+
+fs.writeFileSync('./export/index.json', JSON.stringify(indexJSON));
+
+merged.forEach((poll, index) => {
+  fs.writeFileSync(`./export/polls/${ index }.json`, JSON.stringify(poll));
+});
